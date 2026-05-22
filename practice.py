0f52212a145b7d99d907bf5279e3b9f8e8c47663
Huzaifa <shahidhuzaifa28@gmail.com>
2026-05-22 11:21:56 +0500

    created user data structure

--- practice.py
+++ practice.py
@@ -1,1 +1,14 @@
-print("Hello world")
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+sum = a + b
+subtract = a - b
+multiply = a * b
+divide = a / b if b != 0 else "Cannot divide by zero"
+
+print("Sum:", sum)
+print("Subtract:", subtract)
+print("Multiply:", multiply)
+print("Divide:", divide)
+
+print("Hi Cherry-pick")
